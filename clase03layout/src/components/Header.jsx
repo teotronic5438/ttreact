518cf5b1ee5451efebaa68b673c1c0002a7c0f01
teotronic5438 <elias.orihuela>
2025-04-21 00:02:46 -0300

    clase 03 layout

---
+++ clase03layout/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header style={{backgroundColor: 'lightblue', padding: '10px', textAlign: 'center', color: 'white'}}>
+            <h1>Bienvenidos a mi App React</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ clase03layout/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <nav style={{ backgroundColor: '#333', color: 'white', padding: '10px'}}>
+      <ul style={{ listStyle: 'none', display: 'flex', justifyContent: 'space-around', margin: 0}}>
+        <li><a href="#home" style={{color: "white", textDecoration: "none"}}>Inicio</a></li>
+        <li><a href="#about" style={{color: "white", textDecoration: "none"}}>Acerca de</a></li>
+        <li><a href="#contact" style={{color: "white", textDecoration: "none"}}>Contacto</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
