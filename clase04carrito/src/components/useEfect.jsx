04bac3a193215f1147b1c020242fd3f36a0dba22
Elias Orihuela <elias.orihuela@gmail.com>
2025-05-02 14:37:50 -0300

    probando useEfect

---
+++ clase04carrito/src/components/useEfect.jsx
@@ -0,0 +1,5 @@
+// import { use, useEffect } from "react";
+
+// useEffect(() => {}, []); // Se ejecuta una vez al cargar el componente
+// useEffect(() => {}, [variable]); // Se ejecuta cada vez que cambia la variable 
+// useEffect(() => {}, [delendencias]); // Se ejecuta cada vez que cambia la variable de dependencias
