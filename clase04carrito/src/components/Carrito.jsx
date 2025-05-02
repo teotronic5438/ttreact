d8567923c3361cf0004de89e8190bc54b26f6861
Elias Orihuela <elias.orihuela@gmail.com>
2025-05-02 17:13:23 -0300

    agregando cantidad, subtotal y total en carrito

--- clase04carrito/src/components/Carrito.jsx
+++ clase04carrito/src/components/Carrito.jsx
@@ -1,6 +1,12 @@
 import "../styles/Carrito.css";
 
 export default function Carrito({productos}){
+
+    // Calculo el total con reduce
+    const total = productos.reduce((acc, prod) => {
+        return acc + (prod.precio * prod.cantidad);
+    }, 0);
+
     return(
         <div className="carrito-conteiner">
             <h2>Carrito de Compras</h2>
@@ -15,6 +21,7 @@ export default function Carrito({productos}){
                             <th>Descripción</th>
                             <th>Precio</th>
                             <th>Cantidad</th>
+                            <th>SubTotal</th>
                         </tr>
                     </thead>
     
@@ -23,10 +30,17 @@ export default function Carrito({productos}){
                             <tr key={producto.nombre}>
                                 <td>{producto.nombre}</td>
                                 <td>{producto.descripcion}</td>
-                                <td>${producto.precio}</td>
+                                <td>$ {producto.precio}</td>
                                 <td>{producto.cantidad}</td>
+                                <td>$ {(producto.precio*producto.cantidad).toFixed(2)}</td>
                             </tr>
                         ))}
+                        <tr className="total-row">
+                            <td colSpan="4" style={{ textAlign: "right", fontWeight: "bold" }}>
+                                Total:
+                            </td>
+                            <td style={{ fontWeight: "bold" }}>${total.toFixed(2)}</td>
+                        </tr>
                     </tbody>
                 </table>)
             }
