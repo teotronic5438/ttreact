db69290651260b4c02e9e458494e7481874c8a86
Elias Orihuela <elias.orihuela@gmail.com>
2025-05-24 18:01:55 -0300

    subiendo pre entrega logica agregar carrito

--- proyectoFinalApp/src/pages/Home.jsx
+++ proyectoFinalApp/src/pages/Home.jsx
@@ -9,35 +9,35 @@ function Home(){
             nombre: "Producto A",
             descripcion: "Descripción del Producto A",
             precio: 100.00,
-            imagen: "https://placehold.co/150x200"
+            imagen: "https://placehold.co/150x100"
         },
         {
             id: 2,
             nombre: "Producto B",
             descripcion: "Descripción del Producto B",
             precio: 150.50,
-            imagen: "https://placehold.co/150x200"
+            imagen: "https://placehold.co/150x100"
         },
         {
             id: 3,
             nombre: "Producto C",
             descripcion: "Descripción del Producto C",
             precio: 75.25,
-            imagen: "https://placehold.co/150x200"
+            imagen: "https://placehold.co/150x100"
         },
         {
             id: 4,
             nombre: "Producto D",
             descripcion: "Descripción del Producto D",
             precio: 200.00,
-            imagen: "https://placehold.co/150x200"
+            imagen: "https://placehold.co/150x100"
         },
         {
             id: 5,
             nombre: "Producto E",
             descripcion: "Descripción del Producto E",
             precio: 50.00,
-            imagen: "https://placehold.co/150x200"
+            imagen: "https://placehold.co/150x100"
         }
     ];
 
